97f6925d0f69d5313b36d9c0d27164082307a2c6
Sumit Mitra <sm1282713@gmail.com>
2024-05-14 21:00:21 +0530

    new files added

---
+++ Signup-page/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
+import Signup from "./components/Singup"; 
+import Login from "./components/Login";
+
+function App() {
+    const user = localStorage.getItem("token");
+
+    return (
+        <Router>
+            <Routes>
+                {user && <Route path="/" element={<Main />} />} 
+                <Route path="/signup" element={<Signup />} /> 
+                <Route path="/login" element={<Login />} /> 
+                <Route path="/" element={<Navigate replace to="/login" />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
